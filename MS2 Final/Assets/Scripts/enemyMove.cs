693874dabe40d7bdda9ba1b2caa6dd82ea431c54
PFranklinM <paulmallon@Pauls-MBP.wireless.newschool.edu>
2016-04-09 11:12:30 -0400

    MS2 Final Prototype

---
+++ MS2 Final/Assets/Scripts/enemyMove.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMove : MonoBehaviour {
+
+	public GameObject player;
+
+	public GameObject enemy;
+
+	public GameObject reset1;
+	public GameObject reset2;
+
+	public Vector3 startPos;
+	public Vector3 endPos;
+	public Vector3 dir;
+	private float radius = 20f;
+
+	private float moveAmount = 20f;
+
+	public float moveCounter = 0f;
+
+	Vector3 origin;
+
+	private bool moveNormal;
+
+	// Use this for initialization
+	void Start () {
+
+		origin = transform.position;
+
+		moveNormal = true;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 moving = new Vector3 (transform.position.x,
+			transform.position.y,
+			transform.position.z);
+
+		if (Vector3.Distance (enemy.transform.position, player.transform.position) < radius) {
+
+			moveNormal = false;
+
+			dir.Normalize ();
+
+			Vector3 playerPos = player.transform.position;
+
+			startPos = enemy.transform.position;
+
+			endPos = playerPos;
+
+			dir = endPos - startPos;
+
+			transform.position += dir * Time.deltaTime * 4.5f;
+
+		} else if(moveNormal == true) {
+
+			moveCounter += Time.deltaTime;
+
+			if (moveCounter > 1) {
+				moveCounter = 0;
+				moveAmount = -moveAmount;
+			}
+
+			moving.x += moveAmount * Time.deltaTime;
+
+			transform.position = moving;
+		}
+
+		if (GameObject.Find ("Player").GetComponent<playerMove> ().bot) {
+
+			moveNormal = true;
+
+			moveCounter += Time.deltaTime;
+
+			if (moveCounter > 1) {
+				moveCounter = 0;
+				moveAmount = -moveAmount;
+			}
+
+			moving.x += moveAmount * Time.deltaTime;
+
+			transform.position = moving;
+		}
+
+		if (Vector3.Distance (enemy.transform.position, reset1.transform.position) < 30) {
+				moveNormal = true;
+				transform.position = origin;
+			}
+
+		if (Vector3.Distance (enemy.transform.position, reset2.transform.position) < 30) {
+			moveNormal = true;
+			transform.position = origin;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "player") {
+			enemy.SetActive (false);
+
+			transform.position = origin;
+		}
+	}
+}
---
+++ MS2 Final/Assets/Scripts/playerMove.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+	public GameObject player;
+
+	public float moveAmount = 40f;
+
+	Rigidbody2D rb;
+
+	public bool top;
+	public bool bot;
+
+	public bool grounded;
+	public bool specialGrounded;
+
+	private int enemiesAttached;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D>();
+
+		top = true;
+		bot = false;
+
+		grounded = true;
+		specialGrounded = true;
+
+		enemiesAttached = 0;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 moving = new Vector3 (transform.position.x,
+			transform.position.y,
+			transform.position.z);
+
+		if (Input.GetKey (KeyCode.A)) {
+			moving.x -= moveAmount * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			moving.x += moveAmount * Time.deltaTime;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && top == true && grounded == true) {
+			rb.velocity = new Vector3(0, 75, 0);
+			grounded = false;
+			specialGrounded = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && bot == true && grounded == true) {
+			rb.velocity = new Vector3(0, -75, 0);
+			grounded = false;
+			specialGrounded = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.W) && grounded == true && specialGrounded == false) {
+			moving.y = 2f;
+			rb.gravityScale = 15;
+
+			top = true;
+			bot = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.S) && grounded == true && specialGrounded == false) {
+			moving.y = -2f;
+			rb.gravityScale = -15;
+
+			top = false;
+			bot = true;
+
+			enemiesAttached = 0;
+		}
+
+		if (enemiesAttached == 0) {
+			moveAmount = 40;
+		}
+
+		if (enemiesAttached == 1) {
+			moveAmount = 20;
+		}
+
+		if (enemiesAttached == 2) {
+			moveAmount = 10;
+		}
+
+		transform.position = moving;
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "ground") {
+			grounded = true;
+			specialGrounded = false;
+		}
+
+		if (coll.gameObject.tag == "ground2") {
+			specialGrounded = true;
+			grounded = true;
+		}
+
+		if (coll.gameObject.tag == "enemy") {
+
+			enemiesAttached++;
+		}
+
+		if (coll.gameObject.tag == "death") {
+			Application.LoadLevel ("GameLose");
+		}
+//
+//		if (coll.gameObject.tag == "win") {
+//			Application.LoadLevel ("Win");
+//		}
+	}
+
+//	void OnCollisionExit2D (Collision2D coll) {
+//		grounded = false;
+//	}
+}
